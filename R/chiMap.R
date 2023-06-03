70177415480118b40e410bef748de0689c90c92b
Salahub <cdsalahub@gmail.com>
2023-06-03 17:34:20 -0400

    Updated chiMap to work on shared computing, finished simulation

--- R/chiMap.R
+++ R/chiMap.R
@@ -27,35 +27,46 @@ preparams <- data.frame(a = rep(aseq, times = length(m1seq)),
                         m1 = rep(m1seq, each = length(aseq)),
                         logD = rep(logD, times = length(m1seq)))
 params <- data.frame(preparams, k = rep(kseq, each = nrow(preparams)))
-altSeq <- lapply(seq_len(nrow(params)), # generate alternatives
-                 function(ii) {
-                     with(params, pgenMix(a[ii], w[ii], m1[ii]))
-                 })
-poolSeq <- lapply(seq_len(nrow(params)), # generate pool functions
-                  function(ii) {
-                      function(p) poolChi(p, k = params$k[ii])
-                  })
+#altSeq <- lapply(seq_len(nrow(params)), # generate alternatives
+#                 function(ii) {
+#                     with(params, pgenMix(a[ii], w[ii], m1[ii]))
+#                 })
+#poolSeq <- lapply(seq_len(nrow(params)), # generate pool functions
+#                  function(ii) {
+#                      function(p) poolChi(p, k = params$k[ii])
+#                  })
 ## control data set generation with random seeds
-seeds <- round(runif(nrow(params), min = 1e5, max = 1e9))
+##seeds <- round(runif(nrow(params), min = 1e5, max = 1e9))
+
 ## helper to modify powersim
-chiSimPower <- function(altLst, poolLst, nsim, seeds, M) {
-    mapply(powerSim, pcomb = poolLst, pgen = altLst,
-           nsim = nsim, seed = seeds, M = M)
+chiSimPower <- function(parRow, alpha = 0.05) {
+    b <- bwa(parRow[2], parRow[1])
+    rands <- cbind(matrix(rbeta(parRow[3]*nsim, parRow[1], b),
+                          ncol = parRow[3], nrow = nsim),
+                   matrix(runif((Mval-parRow[3])*nsim),
+                          ncol = Mval - parRow[3], nrow = nsim))
+    pcomb <- apply(rands, 1, poolChi, k = parRow[5])
+    pow <- mean(pcomb <= alpha)
+    pow
 }
 
 ## get the map for the chi method (requires parallel computing)
 library(parallel)
-ncores <- ceiling(detectCores()*0.8)
-spltInds <- rep(1:ncores, # indices for splitting functions
-                each = ceiling(nrow(params)/ncores))[1:nrow(params)]
-ncores <- spltInds[length(spltInds)] # fix number of cores
-sdLst <- split(seeds, spltInds) # split seeds
-altLst <- split(altSeq, spltInds)
-poolLst <- split(poolSeq, spltInds) # functions separated by indices
-powerschi <- mcmapply(chiSimPower, altLst = altLst, poolLst = poolLst,
-                      nsim = nsim, seeds = sdLst, M = Mval,
-                      mc.cores = ncores, mc.preschedule = FALSE,
-                      affinity.list = 1:ncores)
+ncores <- ceiling(detectCores()*0.6)
+clst <- makeCluster(ncores)
+clusterExport(clst, c("bwa", "Mval", "nsim", "poolChi"))
+powerschi <- parRapply(clst, params, chiSimPower)
+stopCluster(clst)
+#spltInds <- rep(1:ncores, # indices for splitting functions
+#                each = ceiling(nrow(params)/ncores))[1:nrow(params)]
+#ncores <- spltInds[length(spltInds)] # fix number of cores
+#sdLst <- split(seeds, spltInds) # split seeds
+#altLst <- split(altSeq, spltInds)
+#poolLst <- split(poolSeq, spltInds) # functions separated by indices
+#powerschi <- mcmapply(powerSim, pcomb = poolSeq, pgen = altSeq,
+#                      nsim = nsim, seed = seeds, M = Mval,
+#                      mc.cores = ncores) #nm = 1:ncores
+                      ##mc.preschedule = FALSE, affinity.list = 1:ncores)
 
 ## store the simulation results
 chiPowers <- list(pars = params,
